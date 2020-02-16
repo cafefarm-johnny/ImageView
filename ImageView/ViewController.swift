21a781323ad2cd5918728bf25224ad446c67f309
Johnny <npcdja@gmail.com>
2020-02-16 21:57:00 +0900

    Initial Commit

---
+++ ImageView/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ImageView
+//
+//  Created by Johnny Uhm on 2020/02/16.
+//  Copyright © 2020 Jamong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btmResize: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+    }
+}
+
